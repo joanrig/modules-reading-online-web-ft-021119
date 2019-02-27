44fec9f61d4afae04e0a5de7a10c04b17028a437
Joan Lyness <joanrigdon@gmail.com>
2019-02-27 05:53:45 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-
+require_relative './class_methods_fancy_dance.rb'
 
 class Dancer
   include Dance
@@ -10,8 +10,6 @@ class Dancer
   def initialize(name)
     @name = name
   end
-
-  @@FancyDance
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 end
