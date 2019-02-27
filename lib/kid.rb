ff784d0f3ab0eceb627ff6fc5d6ddebe8205e7f2
Joan Lyness <joanrigdon@gmail.com>
2019-02-27 05:41:49 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,10 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  attr_accessor :name
   include Dance
 
-  attr_accessor(name)
+  attr_accessor :name
 
   def initialize(name)
     @name = name
